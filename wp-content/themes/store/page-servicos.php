14f25cfe881ff259317026657af8388704e44fb3
FabianoFaria <fabiano@joiss.com.br>
2015-08-06 01:29:55 -0300

    adicao de conteudo na pagina de servicos

--- wp-content/themes/store/page-servicos.php
+++ wp-content/themes/store/page-servicos.php
@@ -12,8 +12,218 @@
 		<div class="row">
 			<div class="col-md-12 servicos_box">
 
-				
+				<div class="portfolio__items">
 
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+					
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+					<div class="col-xs-6 col-sm-4 col-md-3">
+						
+							<div class="portfolio-item__img">
+
+								<a data-lightbox="portfolio" href="img/portfolio_1.jpg">
+									<img class="img-responsive" alt="..." src="<?php echo get_template_directory_uri(); ?>/fix_images/estante_a.jpg"></img>
+								</a>
+
+								<div class="portfolio-item__mask">
+									<div class="portfolio-item-mask__content">
+										<div class="portfolio-item-mask__title">
+											Estante de loja A
+										</div>
+										<div class="portfolio-item-mask__summary">
+											Pellentesque metus arcu, placerat nec porta in, ultricies id enim. Nullam sit amet viverra sapien, quis aliquet quam.
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+
+						
+					</div> <!-- col-xs-6  -->
+
+
+
+
+
+				</div> <!-- Portifolio__itens -->
 
 
 
