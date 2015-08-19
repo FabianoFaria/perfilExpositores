4fafda79935236881a57d8984428a9cbf7002f16
FabianoFaria <fabiano@joiss.com.br>
2015-08-18 23:26:34 -0300

    página de contatos criada

--- wp-content/themes/store/page-fale_conosco.php
+++ wp-content/themes/store/page-fale_conosco.php
@@ -11,6 +11,17 @@
 
 		<div class="row">
 			<div class="col-md-12 contact_box">
+				
+				<div class="col-md-6">
+					<?php echo do_shortcode('[contact-form-7 id="27" title="Formulário de contato 1"]'); ?>
+				</div>
+
+				<div class="col-md-6">
+				</div>
+
+
+
+				
 
 			</div>
 		</div>
